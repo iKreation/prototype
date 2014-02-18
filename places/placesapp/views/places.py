dbfcfa911c65277d30aca1e76236ea3477bf9513
mdiasloureiro <mdiasloureiro@gmail.com>
2014-02-18 15:53:36 +0000

    crud finished

--- places/placesapp/views/places.py
+++ places/placesapp/views/places.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.template import RequestContext, loader
 from placesapp.models import *
+import simplejson as json
+from django.views.decorators.csrf import csrf_exempt
 
 def hello(request, ident, category):
 	obj = {} # create dict
@@ -19,13 +21,18 @@ def list(request):
 	# return http response
 
 	query = Places.objects.all()
-	obj = {}
-	obj['query'] = query
+	lista = []
 
-	t = loader.get_template('list_view.html')
-	return HttpResponse(t.render(RequestContext(request, obj)))
+	for l in query:		
+		new_obj = {}
+		new_obj['title'] = l.title
+		new_obj['description'] = l.description
+		new_obj['coordinate'] = l.coordinate
+		lista.append(new_obj)
 
+	return HttpResponse(json.dumps(lista), content_type='json')
 
+@csrf_exempt
 def create(request):
 	# grant it's a post
 	# read the POSTed data
@@ -42,18 +49,80 @@ def create(request):
 								  description = description, 
 								  coordinate = coordinate)
 
-			return HttpResponseRedirect('/')
+			return HttpResponse(json.dumps({
+									'success': True, 
+									'msg': 'Success'
+								}), content_type='json')
 		except:
-			return HttpResponseRedirect('/')
-
+			return HttpResponse(json.dumps({
+									'success': False, 
+									'msg': 'An error has occurred.'
+								}), content_type='json')
 	else:
-		return HttpResponseRedirect('/')
+		return HttpResponse(json.dumps({
+									'success': False, 
+									'msg': 'Invalid request method.'
+								}), content_type='json')
 
 def get(request,ident):
-	pass
+	# query the database with ident 
+	# verify if exists
+	# json
 
+	try:
+		p = Places.objects.get(id = ident)
+		obj = {}
+		obj['id'] = p.id
+		obj['title'] = p.title
+		return HttpResponse(json.dumps(obj), content_type="json")
+	except:
+		return HttpResponse(json.dumps({
+									'success': False, 
+									'msg': 'Does not exist.'
+								}), content_type='json')
+
+@csrf_exempt
 def update(request, ident):
-	pass
+	# find the object and check if exists
+ 	# update the object
+
+ 	if request.method == 'POST':
+
+ 		title = request.POST['title']
+		description = request.POST['description']
+		coordinate = request.POST['coordinate']
+
+	 	try:
+	 		p = Places.objects.get(id = ident)
+	 		p.title = title
+	 		p.description = description
+	 		p.coordinate = coordinate
+	 		p.save()
+
+	 		return HttpResponse(json.dumps({'success': True}), 
+	 				content_type="json")
+	 	except:
+	 		return HttpResponse(json.dumps({
+									'success': False, 
+									'msg': 'Does not exist.'
+								}), content_type='json')
+	
+	else:
+		return HttpResponse(json.dumps({
+										'success': False, 
+										'msg': 'Invalid request method.'
+									}), content_type='json')
+
 
 def delete(request, ident):
-	pass
+	try:
+		p = Places.objects.get(id = ident)
+		p.delete()
+
+		return HttpResponse(json.dumps({'success': True}), 
+	 				content_type="json")
+	except:
+ 		return HttpResponse(json.dumps({
+								'success': False, 
+								'msg': 'Does not exist.'
+							}), content_type='json')
