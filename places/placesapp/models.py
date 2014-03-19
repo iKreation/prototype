00900715b24903d2678562ef63bd90177876cd61
coachi <hsanchofilho@gmail.com>
2014-03-19 18:21:17 +0000

    category migration

--- places/placesapp/models.py
+++ places/placesapp/models.py
@@ -4,12 +4,17 @@ from django.conf import settings
 
 # Create your models here.
 
+class Category(models.Model):
+
+	title = models.CharField(max_length=250)
+
 class Places(models.Model):
 
     user = models.ForeignKey(User)
     coordinate = models.CharField(max_length=75)
     title = models.CharField(max_length=250)
     description = models.CharField(max_length=2000)
+    category = models.ForeignKey(Category)
 
 class Photos(models.Model):
 
